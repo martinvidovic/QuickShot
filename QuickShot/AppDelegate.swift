f4b8caa0fc96886c58cc55ac48ffdd47514ba6a2
martinvidovic <martinvidovic@me.com>
2020-07-14 11:06:57 +0200

    Create Popover and lost of focus.

--- QuickShot/AppDelegate.swift
+++ QuickShot/AppDelegate.swift
@@ -14,22 +14,55 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
 
-
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+    var eventMonitor: EventMonitor?
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
-
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        createPopover(view: contentView)
+        createStatusBar()
+        NSApp.activate(ignoringOtherApps: true)
+        
+        eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [unowned self] event in
+            if self.popover.isShown {
+                self.closePopover(sender: event)
+            }
+        }
     }
-
+    
+    func createPopover(view: ContentView) {
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 350, height: 250)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: view)
+        self.popover = popover
+    }
+   
+    func createStatusBar() {
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = #selector(togglePopover(_:))
+        }
+    }
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                closePopover(sender: sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+                eventMonitor?.start()
+            }
+        }
+    }
+    
+    func closePopover(sender: Any?) {
+        popover.performClose(sender)
+        eventMonitor?.stop()
+    }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
